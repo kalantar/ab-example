0fe7452fa605ba44a2df70affa44f8b37e40631b
Michael Kalantar <kalantar@us.ibm.com>
2022-11-15 15:59:50 -0500

    log response
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- samples/abn-sample/backend/main.go
+++ samples/abn-sample/backend/main.go
@@ -23,6 +23,7 @@ func recommend(w http.ResponseWriter, req *http.Request) {
 		Name:   "sample",
 		Source: os.Getenv("HOSTNAME"),
 	}
+	Logger.Info("/recommend returning ", data)
 	json.NewEncoder(w).Encode(data)
 }
 
